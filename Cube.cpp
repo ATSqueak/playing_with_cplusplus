8d98126102c1741ae32bd811e014406794262f6b
ATSqueak <arif_taha@yahoo.com>
2022-03-01 11:49:45 +0000

    Create Cube.cpp
    cubing a number

---
+++ Cube.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+double cube (double num){
+    return num*num*num;
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    cout << cube(5.0) << endl;
+
+    return 0;
+}
