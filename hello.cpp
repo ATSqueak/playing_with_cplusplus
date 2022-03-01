6403f1da325c540e057fee96ff6588c1b5f0fbd7
ATSqueak <arif_taha@yahoo.com>
2022-03-01 11:13:31 +0000

    Create hello.cpp
    String output and using variables

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "Hello World!" << endl;
+    cout << " /___|" << endl;
+    cout << "    /|" << endl;
+    cout << "   / |" << endl;
+    cout << "  /  |" << endl;
+    string characterName = "Clesdy";
+    int characterAge;
+    characterAge = 55;
+    cout << "There was someone name " << characterName << endl;
+    cout << "They were " << characterAge << " years old" << endl;
+    characterName = "Mule";
+    cout << "They liked the name " << characterName << endl;
+    cout << "But not being " << characterAge << endl;
+    return 0;
+}
