b03c97cd7ef783212601511074a3b828c389ef9a
ATSqueak <arif_taha@yahoo.com>
2022-03-01 13:00:53 +0000

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -18,5 +18,25 @@ int main(int argc, const char * argv[]) {
     characterName = "Mule";
     cout << "They liked the name " << characterName << endl;
     cout << "But not being " << characterAge << endl;
+    
+    //Using Hex for ASCII values
+    cout << "\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64"<<endl;
+    
+    //Set width
+    cout << setw(10) << 10+20/(10-5) << endl;
+    
+    //Different int types
+    short int bean = 5;
+    int bean1 = 1;
+    long int bean2 = 3;
+    long earth_diameter = 12756000L;
+    signed int negat = -9;
+    
+    cout << bean << endl;
+    cout << bean1 << endl;
+    cout << bean2 << endl;
+    cout << earth_diameter << endl;
+    cout << negat << endl;
+    
     return 0;
 }
