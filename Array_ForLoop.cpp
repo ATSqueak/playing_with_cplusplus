99c0460c3647e43ded020f4074edda7d59082014
ATSqueak <arif_taha@yahoo.com>
2022-03-01 12:52:51 +0000

    Create Array_ForLoop.cpp

---
+++ Array_ForLoop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    int  nums[] = {1,2,5,7,3};
+    
+    cout << sizeof(nums);
+    
+    for(int i=0 ; i < 5; i++){
+        cout << nums[i] << endl;
+        }
+    
+    return 0;
+}
