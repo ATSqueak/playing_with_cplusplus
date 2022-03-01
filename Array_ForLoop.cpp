226cd74cff370ef01c479677223a864593285a61
ATSqueak <arif_taha@yahoo.com>
2022-03-01 12:53:57 +0000

    Update Array_ForLoop.cpp

--- Array_ForLoop.cpp
+++ Array_ForLoop.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+int power(int baseNum, int powNum) {
+    int result = 1;
+    for(int i=0;i<powNum;i++){
+        result = result * baseNum;
+        }
+    
+    return result;
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -13,6 +22,8 @@ int main(int argc, const char * argv[]) {
     for(int i=0 ; i < 5; i++){
         cout << nums[i] << endl;
         }
+    
+    cout << power(4,2) << endl;
     
     return 0;
 }
